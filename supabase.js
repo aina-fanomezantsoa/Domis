71fabd60fb6ed2edb04f18e379bc82f81d62b465
aina <ainafanomezantsoa37@gmail.com>
2026-09-20 06:58:22 +0200

    feature/initialisationSupabase

---
+++ supabase.js
@@ -0,0 +1,4 @@
+
+const supabaseUrl = 'https://vdxnyocmnredcnpnnnto.supabase.co';
+const supabaseKey = 'sb_publishable_ebwYDb00WIrH0o...'; // Mettez votre clé complète ici
+export const supabase = globalThis.supabase.createClient(supabaseUrl, supabaseKey);
